b18526a6dc4164c0cfc5e99dc3948cbb07bcacea
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-08-06 13:55:51 -0300

    add doc em estoque

--- ListMEAPI/ListMEAPI/Controllers/TelaGerenciarCompras/EstoqueController.cs
+++ ListMEAPI/ListMEAPI/Controllers/TelaGerenciarCompras/EstoqueController.cs
@@ -1,6 +1,7 @@
 ﻿using ListMEAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ListMEAPI.Controllers.TelaGerenciarCompras
 {
@@ -10,6 +11,24 @@ namespace ListMEAPI.Controllers.TelaGerenciarCompras
     {
         private ListMEContext _listMEContext = new ListMEContext();
         //POST ESTOQUE
+        /// <summary>
+        /// Cadastra um novo estoque
+        /// </summary>
+        /// <returns>Retorna o estoque recém criada</returns>
+        /// <remarks>
+        /// Exemplo requisição:
+        ///
+        ///     POST /api/Estoque/AdicionarEstoque
+        ///     {
+        ///        
+        ///        
+        ///        
+        ///     }
+        ///
+        /// </remarks>
+        /// <param name="estoque">Modelo do estoque</param>
+        /// <response code="200">Retorna o estoque recém criado</response>
+        /// <response code="500">Ocorreu algum erro criar o estoque</response>
         [HttpPost("AdicionarEstoque")]
 
         public ActionResult<EstoqueModel> AdicionarEstoque(EstoqueModel estoque)
@@ -20,30 +39,79 @@ namespace ListMEAPI.Controllers.TelaGerenciarCompras
         }
 
         //GET ESTOQUE
-        [HttpGet("Receber o estoque")]
-        public ActionResult<EstoqueModel> GetEstoque()
+        /// <summary>
+        /// Listar todo estoque
+        /// </summary>
+        /// <returns>Lista de estoques criados</returns>
+        /// <response code="404">Não há estoque cadastrado</response>
+        /// <response code="200">Retorna a lista de estoques cadastrados</response>
+        /// <response code="500">Ocorreu algum erro ao obter lista de estoquea cadastrados</response>
+        [HttpGet("RequererTodoEstoque")]
+        public ActionResult<List<EstoqueModel>> RequererTodoEstoque()
         {
-            return Ok(_listMEContext.Estoque);
+            var estoque = _listMEContext.Estoque.ToList();
+            if (estoque == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(estoque);
+            }
         }
-       
+
 
         //PUT ESTOQUE
         //Necessário entender como será feita a identificação do estoque
         //Utilizando por enquanto o Id do estoque
-        [HttpPut("Alterar Estoque{Id}")]
+
+        /// <summary>
+        /// Substitui um estoque a partir de sua Id
+        /// </summary>
+        /// <returns></returns>
+        /// <remarks>  
+        /// Exemplo requisição:
+        ///
+        ///     PUT /api/Estoque/SubstituirEstoque{Id}
+        ///     {
+        ///       
+        ///        }
+        ///        ]
+        ///        
+        ///     }
+        ///
+        /// </remarks>
+        /// <param name="Id">Id do usuário</param>
+        /// <param name="estoque">Modelo do usuário</param>
+        /// <response code="400">Usuário não pode ter sua Id modificada</response>
+        /// <response code="404">Usuário não encontrado</response>
+        /// <response code="204">Usuário substituído</response>
+        [HttpPut("SubstituirEstoque{Id}")]
         public ActionResult<EstoqueModel> PutEstoque(int Id, EstoqueModel estoque)
         {
-            var existeEstoque = _listMEContext.Estoque.Find(Id);
-            if(existeEstoque != null)
+            if (Id != estoque.Id_Estoque)
             {
-                //existeEstoque.Produtos = estoque.Produtos;
-                _listMEContext.SaveChanges();
-                return Ok(_listMEContext.Estoque.Find(Id));
+                return BadRequest();
             }
-            return NotFound();
+            else
+            {
+                //Substitui valor da instância no banco de dados 
+                _listMEContext.Entry(estoque).State = EntityState.Modified;
+                _listMEContext.SaveChanges();
+
+                return NoContent();
+            }
         }
+
+        /// <summary>
+        /// Delete o estoque a partir de sua Id
+        /// </summary>
+        /// <returns>Retorna o estoque recém deletado</returns>
+        /// <param name="Id">Id do estoque</param>
+        /// <response code="404">estoque não encontrado</response>
+        /// <response code="204">estoque deletado</response>
         //DELETE ESTOQUE
-        [HttpDelete("Deletar estoque{Id}")]
+        [HttpDelete("DeletarEstoque{Id}")]
         public ActionResult<EstoqueModel> DeleteEstoque(int Id)
         {
             var existeEstoque = _listMEContext.Estoque.Find(Id);
