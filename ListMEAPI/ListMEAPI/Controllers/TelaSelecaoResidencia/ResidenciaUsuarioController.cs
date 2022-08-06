405d842fabc55643f09878b64e94355c045738d5
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-08-06 14:13:09 -0300

    add doc residencia

--- ListMEAPI/ListMEAPI/Controllers/TelaSelecaoResidencia/ResidenciaUsuarioController.cs
+++ ListMEAPI/ListMEAPI/Controllers/TelaSelecaoResidencia/ResidenciaUsuarioController.cs
@@ -10,8 +10,16 @@ namespace ListMEAPI.Controllers.TelaSelecaoResidencia
     public class ResidenciaUsuarioController : ControllerBase
     {
         //GET para retornar todas residências referentes ao usuário X
+
         private ListMEContext _listMEContext = new ListMEContext();
 
+        /// <summary>
+        /// Listar todas residências a partir do usuário
+        /// </summary>
+        /// <returns>Lista de residências a partir do usuário</returns>
+        /// <response code="404">Não há residências a partir do usuário cadastrada</response>
+        /// <response code="200">Retorna a residência cadastrados</response>
+        /// <response code="500">Ocorreu algum erro ao obter residência cadastrados</response>
         [HttpGet("RequererResidencias")]
         public ActionResult<UsuarioModel> RequererTodasResidencias(int IdUsuario)
         {
