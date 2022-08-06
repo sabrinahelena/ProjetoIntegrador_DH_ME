62e33cf486bc6c10c12da99c9ba8406dae69d729
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-08-06 13:36:34 -0300

    add doc em cadastrousuario

--- ListMEAPI/ListMEAPI/Controllers/TelaCadastro/CadastroUsuarioController.cs
+++ ListMEAPI/ListMEAPI/Controllers/TelaCadastro/CadastroUsuarioController.cs
@@ -10,7 +10,36 @@ namespace ListMEAPI.Controllers.TelaCadastro
     public class CadastroUsuarioController : ControllerBase
     {
         private ListMEContext _listMEContext = new ListMEContext();
-
+       
+        /// <summary>
+        /// Cadastra um novo usuário
+        /// </summary>
+        /// <returns>Retorna o usuário recém criado</returns>
+        /// <remarks>
+        /// Exemplo requisição:
+        ///
+        ///     POST /Usuario
+        ///     {
+        ///        "nome_Usuario": "Sabrina",
+        ///        "sobrenome": "Helena",
+        ///        "telefone": "99999999999"
+        ///        "data_Nascimento": "08/01/2004",
+        ///        "email:" "sabrinahelenaf@gmail.com",
+        ///        "foto_Perfil:" "x",
+        ///        "residencias": [
+        ///        {
+        ///        "nome_Residencias": "casa de praia",
+        ///        "descricao_Residencias": "de frente para o mar",
+        ///        "foto_Residencias": x"
+        ///        }
+        ///       ]
+        ///   
+        ///     }
+        ///
+        /// </remarks>
+        /// <param name="usuario">Modelo do usuário</param>
+        /// <response code="200">Retorna o usuário recém criado</response>
+        /// <response code="500">Ocorreu algum erro criar o usuários</response>
         //POST USUARIO
 
         [HttpPost("AdicionarUsuario")]
