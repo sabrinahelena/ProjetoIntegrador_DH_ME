d9b9cde8c6c1e60bf5b9e79ac06074b5ff9fa7c7
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-08-07 13:31:43 -0300

    alteração model residencia

--- ListMEAPI/ListMEAPI/Models/ResidenciaModel.cs
+++ ListMEAPI/ListMEAPI/Models/ResidenciaModel.cs
@@ -1,11 +1,26 @@
-﻿namespace ListMEAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ListMEAPI.Models
 {
     public class ResidenciaModel
     {
+        public ResidenciaModel() { }
+
+        public ResidenciaModel(string nome_Residencias, string? descricao_Residencias, string? foto_Residencias)
+        {
+            Nome_Residencias = nome_Residencias;
+            Descricao_Residencias = descricao_Residencias;
+            Foto_Residencias = foto_Residencias;
+        }
+
+        [Key]
         public int Id_Residencias { get; set; }
         public string Nome_Residencias { get; set; }
         public string? Descricao_Residencias { get; set; }
         public string? Foto_Residencias { get; set; }
+
+        public UsuarioModel usuario { get; set; }
+
 
     }
 }
