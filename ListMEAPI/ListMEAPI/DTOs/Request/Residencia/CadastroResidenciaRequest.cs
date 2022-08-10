af897b11c324e9fb9c2b56ede5a9625efce17f25
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-08-10 13:20:03 -0300

    att residencia

--- ListMEAPI/ListMEAPI/DTOs/Request/Residencia/CadastroResidenciaRequest.cs
+++ ListMEAPI/ListMEAPI/DTOs/Request/Residencia/CadastroResidenciaRequest.cs
@@ -2,8 +2,17 @@
 {
     public class CadastroResidenciaRequest
     {
+        /// <summary>
+        /// A descrição para identificar a residência
+        /// </summary>
         public string Nome_Residencias { get; set; }
         public string? Descricao_Residencias { get; set; }
+        /// <summary>
+        /// A foto da residência
+        /// </summary>
         public string? Foto_Residencias { get; set; }
+        /// <summary>
+        /// Toda residência possui um estoque.
+        /// </summary>
     }
 }
