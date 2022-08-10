e6ea3a1b937c4f8f87d1ad8af9a7062b6be00d76
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-08-10 13:13:20 -0300

    add doc contato

--- ListMEAPI/ListMEAPI/Models/ContatoModel.cs
+++ ListMEAPI/ListMEAPI/Models/ContatoModel.cs
@@ -13,8 +13,18 @@ namespace ListMEAPI.Models
 
         [Key]
         public int Id_Contato { get; set; }
+
+        /// <summary>
+        /// O nome da pessoa que quer entrar em contato.
+        /// </summary>
         public string Nome { get; set; }
+        /// <summary>
+        /// O e-mail da pessoa que quer entrar em contato.
+        /// </summary>
         public string Email { get; set; }
+        /// <summary>
+        /// A mensagem da pessoa que quer entrar em contato.
+        /// </summary>
         public string Mensagem { get; set; }
     }
 }
