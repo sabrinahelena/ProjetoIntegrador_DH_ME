ee939bd2ba4e6d5c97b60eeccd992c808c9407b2
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-08-10 13:20:34 -0300

    doc em acesso

--- ListMEAPI/ListMEAPI/DTOs/Request/Login/CadastroAcessoRequest.cs
+++ ListMEAPI/ListMEAPI/DTOs/Request/Login/CadastroAcessoRequest.cs
@@ -1,8 +1,18 @@
-﻿namespace ListMEAPI.DTOs.Request.Login
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ListMEAPI.DTOs.Request.Login
 {
     public class CadastroAcessoRequest
     {
+        /// <summary>
+        /// O e-mail para autenticar.
+        /// </summary>
         public string email { get; set; }
+        /// <summary>
+        /// A senha para autenticar.
+        /// </summary>
+        /// 
+        [MinLength(4, ErrorMessage = "A senha deve conter ao menos 4 dígitos.")]
 
         public string senha { get; set; }
     }
