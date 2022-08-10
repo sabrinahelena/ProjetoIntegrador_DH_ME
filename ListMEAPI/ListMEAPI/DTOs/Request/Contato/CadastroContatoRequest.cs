8bffa32493e677837268e0fa4b224dde307fbbbc
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-08-10 13:23:08 -0300

    doc

--- ListMEAPI/ListMEAPI/DTOs/Request/Contato/CadastroContatoRequest.cs
+++ ListMEAPI/ListMEAPI/DTOs/Request/Contato/CadastroContatoRequest.cs
@@ -7,8 +7,17 @@
          * É um padrão de projeto de software usado para transferir dados 
          * entre subsistemas de um software.
          */
+        /// <summary>
+        /// O nome da pessoa que quer entrar em contato.
+        /// </summary>
         public string Nome { get; set; }
+        /// <summary>
+        /// O e-mail da pessoa que quer entrar em contato.
+        /// </summary>
         public string Email { get; set; }
+        /// <summary>
+        /// A mensagem da pessoa que quer entrar em contato.
+        /// </summary>
         public string Mensagem { get; set; }
 
         //ResidênciaModel
