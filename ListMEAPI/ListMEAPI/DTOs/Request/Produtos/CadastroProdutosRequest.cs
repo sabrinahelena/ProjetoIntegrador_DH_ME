3ff71eaecc64dc7a90e80c2be045b9aa98b172d7
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-08-10 13:26:19 -0300

    att body produto

--- ListMEAPI/ListMEAPI/DTOs/Request/Produtos/CadastroProdutosRequest.cs
+++ ListMEAPI/ListMEAPI/DTOs/Request/Produtos/CadastroProdutosRequest.cs
@@ -1,4 +1,6 @@
-﻿namespace ListMEAPI.DTOs.Request.Produtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ListMEAPI.DTOs.Request.Produtos
 {
     public class CadastroProdutosRequest
     {
@@ -14,5 +16,15 @@
         /// Preço do produto
         /// </summary>
         public float Preco { get; set; }
+        /// <summary>
+        /// A data de validade do produto
+        /// </summary>
+        [MinLength(8, ErrorMessage = "A data de validade deve conter pelo menos 8 dígitos, considerando DIA (XX), MÊS (XX), e ANO (XXXX)")]
+        public string? Data_Validade { get; set; }
+
+        /// <summary>
+        /// A quantidade existente do produto
+        /// </summary>
+        public int? Quantidade_Produto { get; set; }
     }
 }
