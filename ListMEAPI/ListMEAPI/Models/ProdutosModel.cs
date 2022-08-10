227272cc072b6d992eae790f1cef9d5a778ea91d
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-08-10 13:12:23 -0300

    add doc produto model

--- ListMEAPI/ListMEAPI/Models/ProdutosModel.cs
+++ ListMEAPI/ListMEAPI/Models/ProdutosModel.cs
@@ -13,11 +13,27 @@ namespace ListMEAPI.Models
 
         [Key]
         public int Id_Produtos { get; set; }
+        /// <summary>
+        /// Nome do produto
+        /// </summary>
         public string Nome_Produtos { get; set; }
+        /// <summary>
+        /// Descrição do produto
+        /// </summary>
         public string? Descricao_Produtos { get; set; }
+        /// <summary>
+        /// Preço do produto
+        /// </summary>
         public float Preco { get; set; }
+        /// <summary>
+        /// A data de validade do produto
+        /// </summary>
+        [MinLength(8, ErrorMessage = "A data de validade deve conter pelo menos 8 dígitos, considerando DIA (XX), MÊS (XX), e ANO (XXXX)")]
         public string? Data_Validade { get; set; }
 
+        /// <summary>
+        /// A quantidade existente do produto
+        /// </summary>
         public int? Quantidade_Produto { get; set; }
 
     }
