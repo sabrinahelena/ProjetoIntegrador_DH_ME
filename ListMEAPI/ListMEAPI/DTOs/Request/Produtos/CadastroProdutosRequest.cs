53332be1be9bdcef71b8138aeeb30f0e7fd34194
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-08-10 13:21:25 -0300

    doc produto

--- ListMEAPI/ListMEAPI/DTOs/Request/Produtos/CadastroProdutosRequest.cs
+++ ListMEAPI/ListMEAPI/DTOs/Request/Produtos/CadastroProdutosRequest.cs
@@ -2,8 +2,17 @@
 {
     public class CadastroProdutosRequest
     {
+        /// <summary>
+        /// Nome do produto
+        /// </summary>
         public string Nome_Produtos { get; set; }
+        /// <summary>
+        /// Descrição do produto
+        /// </summary>
         public string? Descricao_Produtos { get; set; }
+        /// <summary>
+        /// Preço do produto
+        /// </summary>
         public float Preco { get; set; }
     }
 }
