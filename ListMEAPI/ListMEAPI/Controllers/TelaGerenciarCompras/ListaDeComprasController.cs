3f98df1b75ea9dd9b6cec33bfd46af679de7bc6d
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-08-21 15:20:01 -0300

    att listacompras

--- ListMEAPI/ListMEAPI/Controllers/TelaGerenciarCompras/ListaDeComprasController.cs
+++ ListMEAPI/ListMEAPI/Controllers/TelaGerenciarCompras/ListaDeComprasController.cs
@@ -15,25 +15,6 @@ namespace ListMEAPI.Controllers.TelaGerenciarCompras
         {
             _listaDeComprasService = listaService;
         }
-
-        /// <summary>
-        /// Retorna todos os produtos na lista de compras da residênci a partir da Id da residência
-        /// </summary>
-        /// <returns>Retorna todos os produtos na lista de compras da residênci a partir da Id da residência</returns>
-        /// <param name="Id">Id do usuário</param>
-        /// <response code="404">Usuário não encontrado</response>
-        /// <response code="200">Retorna usuário encontrado</response>
-        [HttpGet("Get Lista de compras por Id de residência")]
-        public ActionResult<List<EstoqueModel>> Listar(int IdResidencia)
-        {
-            var existe = _listaDeComprasService.ListarAListaDeCompras(IdResidencia);
-            if(existe == null)
-            {
-                return BadRequest(new { message = "Rêsidencia não encontrada" });
-            }
-            return Ok(existe);
-        }
-
         //ADICIONAR MAIS PRODUTOS NA LISTA DE COMPRAS DA RESIDÊNCIA
         /// <summary>
         /// Cadastra uma novo produto na lista de compras
@@ -48,8 +29,8 @@ namespace ListMEAPI.Controllers.TelaGerenciarCompras
         /// <param name="IdResidencia">Id da residência</param>
         /// <response code="200"></response>
         /// <response code="500">Ocorreu algum erro ao adicionar o produto na lista</response>
-        [HttpPost]
-        public ActionResult CriarLista(int IdResidencia, int IdProduto)
+        [HttpPost("AdicionarListaDeCompras")]
+        public ActionResult AdicionarListaDeCompras(int IdResidencia, int IdProduto)
         {
             var boolean = _listaDeComprasService.CadastrarProdutoNaLista(IdResidencia, IdProduto);
             if (boolean)
@@ -61,6 +42,49 @@ namespace ListMEAPI.Controllers.TelaGerenciarCompras
                 return BadRequest();
             }
         }
+
+        /// <summary>
+        /// Retorna todos os produtos na lista de compras da residênci a partir da Id da residência
+        /// </summary>
+        /// <returns>Retorna todos os produtos na lista de compras da residênci a partir da Id da residência</returns>
+        /// <param name="Id">Id do usuário</param>
+        /// <response code="404">Usuário não encontrado</response>
+        /// <response code="200">Retorna usuário encontrado</response>
+        [HttpGet("RequererListaDeComprasPorIdResidência")]
+        public ActionResult<List<EstoqueModel>> Listar(int IdResidencia)
+        {
+            var existe = _listaDeComprasService.ListarAListaDeCompras(IdResidencia);
+            if(existe == null)
+            {
+                return BadRequest(new { message = "Rêsidencia não encontrada" });
+            }
+            return Ok(existe);
+        }
+
+        /// <summary>
+        /// Deleta um produto da lista a partir de sua Id
+        /// </summary>
+        /// <returns>Retorna se o estoque foi deletado ou não</returns>
+        /// <param name="IdProduto">Id do estoque</param>
+        /// <param name="IdResidencia">Id do estoque</param>
+        /// <response code="404">Produto ou residência não encontrado</response>
+        /// <response code="204">Produto deletado da lista de compras da residência</response>
+        [HttpDelete("DeletarProdutoDaListaPorId")]
+        public ActionResult DeletarProdutoDaListaPorId(int IdProduto, int IdResidencia)
+        {
+            var boolean = _listaDeComprasService.DeletarProdutoLista(IdResidencia, IdProduto);
+            if (boolean)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+        }
+
+
 
         //ALTERAR QUANTIDADE DO PRODUTO NA LISTA DE COMPRAS DA RESIDÊNCIA
         /// <summary>
@@ -78,8 +102,8 @@ namespace ListMEAPI.Controllers.TelaGerenciarCompras
         /// <param name="Quantidade">Quantidade a ser adicionada ou retirada</param>
         /// <response code="200">A quantidade foi alterada com sucesso</response>
         /// <response code="500">Ocorreu algum erro ao alterar a quantidade</response>
-        [HttpPatch("Alterar quantidade de um produto na lista de compras")]
-        public ActionResult AlterarProdutoNaLista(int IdResidencia,int IdProduto,int Quantidade )
+        [HttpPatch("AtualizarQuantidadeProdutoNaListaDeCompras")]
+        public ActionResult AtualizarQuantidadeProdutoNaListaDeCompras(int IdResidencia,int IdProduto,int Quantidade )
         {
             var boolean = _listaDeComprasService.AlterarQuantidade(IdResidencia, IdProduto, Quantidade);
             if (boolean)
@@ -92,27 +116,6 @@ namespace ListMEAPI.Controllers.TelaGerenciarCompras
             }
         }
 
-        /// <summary>
-        /// Deleta um produto da lista a partir de sua Id
-        /// </summary>
-        /// <returns>Retorna se o estoque foi deletado ou não</returns>
-        /// <param name="IdProduto">Id do estoque</param>
-        /// <param name="IdResidencia">Id do estoque</param>
-        /// <response code="404">Produto ou residência não encontrado</response>
-        /// <response code="204">Produto deletado da lista de compras da residência</response>
-        [HttpDelete]
-        public ActionResult DeletarProdutoDaLista(int IdProduto,int IdResidencia)
-        {
-            var boolean = _listaDeComprasService.DeletarProdutoLista(IdResidencia, IdProduto);
-            if (boolean)
-            {
-                return Ok();
-            }
-            else
-            {
-                return BadRequest();
-            }
-            
-        }
+       
     }
 }
