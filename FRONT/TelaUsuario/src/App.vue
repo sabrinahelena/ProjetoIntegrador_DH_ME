70463dacbe3d2316d7167718d414bf334536ef11
Danilo Hiroyuki Sanada <danilohsanada@hotmail.com>
2022-08-22 14:37:00 -0300

    Adicionando o vue

---
+++ FRONT/TelaUsuario/src/App.vue
@@ -0,0 +1,24 @@
+
+
+<template>
+  <div>
+    <MenuLateral />
+  </div>
+  
+</template>
+
+<script>
+import MenuLateral from './components/MenuLateral.vue';
+export default{
+  name:`App`,
+  components:{
+    MenuLateral
+  }
+}
+</script>
+
+<style>
+  body{
+    background-color: aqua;
+  }
+</style>
---
+++ FRONT/TelaUsuario/src/components/MenuLateral.vue
@@ -0,0 +1,50 @@
+
+<template>
+  <div id="menu-items">
+    
+    <ul>
+      <h2><b>ListMe</b></h2>
+      <li><a href="/">Home</a></li>
+      <li><a href="/">Configurações</a></li>
+      <li><a href="/">Gerenciar Compras</a></li>
+    </ul>
+    
+  </div>
+</template>
+<script>
+export default{
+  name:`MenuLateral`
+}
+</script>
+
+<style scoped>
+#menu-items{
+  position: fixed;
+  top:0;
+  left:0;
+  background-color: #2E4756;
+  width: 25%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  text-align: left;
+  align-items: center;
+}
+
+ul{
+  list-style: none;
+  text-align: left;
+  align-items: left;
+  color:white;
+}
+
+ul li{
+  margin: 100px 0px;
+  font-size: 25px;
+}
+
+ul li a{
+  color:white;
+}
+</style>
