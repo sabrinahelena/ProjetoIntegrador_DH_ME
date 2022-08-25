833acb8fed21af2574b692668c489e05cbcaa394
Danilo Hiroyuki Sanada <danilohsanada@hotmail.com>
2022-08-25 17:17:05 -0300

    Adicionando estoque

---
+++ FRONT/TelaUsuario/src/components/Estoque.vue
@@ -0,0 +1,33 @@
+<template>
+    <table border="1">
+        <tr id="Linha-Inicial">
+            <th><b>Produto</b></th>
+            <th><b>Quantidade</b></th>
+            <th><b>Data de Validade</b></th>
+        </tr>
+        <tbody>
+            <tr>
+                <td>teste</td>
+                <td>teste</td>
+                <td>teste</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+
+<style scoped>
+table{
+    position: fixed;
+    width: 70%;
+    right: 20px;
+    top:80px;
+}
+</style>
+
+
+<script>
+export default{
+    name:'Estoque'
+}
+</script>
