18bef9fdd1d46a7b33df84a0526d46110914aa41
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-08-27 15:30:37 -0300

    add responsividade compras

--- FRONT/TelaUsuario/src/components/ListaCompras.vue
+++ FRONT/TelaUsuario/src/components/ListaCompras.vue
@@ -126,5 +126,104 @@ td {
   border-bottom: 1px solid #2E4756;
 }
 
+@media(max-width: 750px){
+  #button-add{
+    /* right: 50px; */
+    margin-top: 20px;
+    top: 150px;
+    display: flex;
+    position: fixed;
+    text-align: center;
+    align-items: center;
+    font-size: 15px;
+   
+}
+
+#mais-image{
+    width: 25px;
+}
+
+
+.add_estoque{
+  margin-left: 90px;
+    font-weight: bold;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+.botao_editarR{
+    margin-left: 10px;
+    background-color: white;
+    border: 3px solid #2E4756;
+    border-radius: 12px;
+    cursor: pointer;
+
+}
+#titulo {
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 70%;
+  height: 100vh;
+  display: flex;
+  text-align: left;
+  font-size: 9px;
+  margin-top: 50px;
+  color: black;
+}
+
+.t{
+  display: flex;
+  justify-content: center;
+  margin-top: 18px;
+}
+
+.titulo-estoque{
+  color: #2E4756;
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    margin-left: 10px;
+    margin-top: 35px;
+
+}
+.estoqueImagem{
+ width: 30px;
+ height: 30px;
+ margin-left: 60px;
+ margin-top: 35px;
+ 
+ }
+
+* {
+  box-sizing: border-box;
+}
+
+
+table {
+  position: fixed;
+  margin-top: 200px;
+  width: 200px;
+  height: 1px;
+  border-collapse: collapse;
+  margin-left: 55px;
+}
+
+table>thead {
+  background-color: #2E4756;
+  color: white;
+  height: 1px;
+}
+
+th,
+td {
+  text-align: center;
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  height: 1px;
+  padding: 1px 1px;
+  font-size: 11px;
+  border-bottom: 1px solid #2E4756;
+}
+
+
+
+
+}
 
 </style>
