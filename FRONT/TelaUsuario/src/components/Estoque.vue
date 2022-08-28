afcc885ce9b375b8ebef59496e79190f31735308
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-08-28 11:57:30 -0300

    alterando modal e add opcoes nas tabelas

--- FRONT/TelaUsuario/src/components/Estoque.vue
+++ FRONT/TelaUsuario/src/components/Estoque.vue
@@ -1,74 +1,64 @@
 <template>
   <div id="titulo">
     <div class="y">
-    <div class="t">
-      <img class="estoqueImagem" src="./imagens/icone-inventario.png">
-      <h1 class="titulo-estoque"><b>Estoque</b></h1>
+      <div class="t">
+        <img class="estoqueImagem" src="./imagens/icone-inventario.png">
+        <h1 class="titulo-estoque"><b>Estoque</b></h1>
+      </div>
+      <div id="button-add">
+        <span class="add_estoque"> Adicionar produto</span>
+        <button v-on:click="Acao()" class="botao_editarR" type="button"><img alt="mais-image" id="mais-image"
+            src="../assets/mais.png"></button>
+      </div>
     </div>
-    <div id="button-add">
-      <span class="add_estoque"> Adicionar produto</span>
-      <button v-on:click="Acao()" class="botao_editarR" type="button"><img alt="mais-image" id="mais-image"
-          src="../assets/mais.png"></button>
-    </div>
-  </div>
 
     <div class="modal">
       <h1 class="form-titulo"> Adicione um produto </h1>
       <div class="form">
-        <form method="get" action="">
-          <div>
-            <input class="input-mensagem" id="primeiro-nome" name="primeiro-nome" type="text"
-              placeholder="Nome produto">
-          </div>
-          <br>
-          <div>
-            <input class="input-mensagem" id="mensagem" name="mensagem" type="text" placeholder="Quantidade">
-          </div>
-          <br>
-          <div>
-            <input class="input-mensagem" id="mensagem" name="mensagem" type="date" placeholder="Data de validade">
-          </div>
-        </form>
+        <main class="lista">
+          <ul>
+            <li>
+              <span class="texto_descricao">PRODUTO
+              <button class="botao_editarR " type="button"><img alt="editar" id="editar"
+                  src="../assets/mais.png"></button></span>
+            </li>
+          </ul>
+        </main>
         <button v-on:click="Fechar()" class="fechar" type="button"><img class="imagem_fechar"
             src="./imagens/icons8-close-60.png" id="mais-image"></button>
-        <button class="botao_adicionar">Adicionar</button>
-
       </div>
 
     </div>
 
     <div class="z">
-    <div id="button-addA">
-      <span class="add_estoque"> Remover produto</span>
-      <button v-on:click="listar" class="botao_editarR" type="button"><img alt="mais-image" id="mais-image"
-          src="./imagens/icons8-remove-60.png"></button>
+      <table>
+        <thead>
+          <th>Produto</th>
+          <th>Quantidade</th>
+          <th>Editar quantidade</th>
+          <th>Remover produto</th>
+        </thead>
+        <tbody>
+          <tr>
+            <th>Maçã</th>
+            <td>3</td>
+            <td>
+              <div id="button-addA">
+              <button v-on:click="" class="botao_editarR" type="button"><img alt="mais-image" id="mais-image"
+                  src="../assets/pencil.png"></button>
+              </div>
+            </td>
+            <td>
+              <div id="button-addA">
+              <button v-on:click="" class="botao_editarR" type="button"><img alt="mais-image" id="mais-image"
+                  src="./imagens/icons8-remove-60.png"></button>
+              </div>
+          </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-    <table>
-      <thead>
-        <th>Produto</th>
-        <th>Quantidade</th>
-        <th>Data de validade</th>
-      </thead>
-      <tbody>
-        <tr>
-          <th>Maçã</th>
-          <td>3</td>
-          <td>20/09/2022</td>
-        </tr>
-        <tr>
-          <th>Banana</th>
-          <td>6</td>
-          <td>21/08/2022</td>
-        </tr>
-        <tr>
-          <th>Leite</th>
-          <td>6</td>
-          <td>21/08/2022</td>
-        </tr>
-      </tbody>
-    </table>
   </div>
-</div>
 </template>
 
 <script>
@@ -96,82 +86,108 @@ export default {
 </script>
 
 <style scoped>
-
-.form-titulo{
-    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-    color: white;
-    text-align: center;
-    margin-top: 30px
-}
-.inputs{
-    width: 250px;
-    border-radius: 4px;
-    border-style: none;
-    height: 20px;
-
-}
-
-.botao_adicionar{
-    margin-left: 60px;
-    background-color: white; 
-    border-radius: 30px;
-    color: #16262E;
-    border-color: #16262E;
-    padding: 15px 32px;
-    text-align: center;
-    text-decoration: none;
-    font-size: 16px;
-    width: auto;
-    height: 50px;
-    font-weight: bold;
+.botao_editarR {
+    background-color: white;
+    border: 3px solid #2E4756;
+    border-radius: 12px;
     cursor: pointer;
-    margin-top: 160px;
+    
+
 }
 
-.input-mensagem{
-    border-radius: 4px;
-    width: 250px;
-    border-style: none;
-    height: 20px;
+#editar{
+  width: 25px;
+}
+.texto_descricao {
+    margin-left: 2px;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    font-size: 18px;
+    color: white;
+    font-weight: bold;
+    
+  }
+.form-titulo {
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  color: white;
+  text-align: center;
+  margin-top: 30px
 }
 
-.form{
-    margin-top: 100px;
-    margin-left: 130px;
+.inputs {
+  width: 250px;
+  border-radius: 4px;
+  border-style: none;
+  height: 20px;
+
+}
+
+ul {
+  list-style: none;
+}
+
+
+.botao_adicionar {
+  margin-left: 60px;
+  background-color: white;
+  border-radius: 30px;
+  color: #16262E;
+  border-color: #16262E;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  font-size: 16px;
+  width: auto;
+  height: 50px;
+  font-weight: bold;
+  cursor: pointer;
+  margin-top: 160px;
+}
+
+.input-mensagem {
+  border-radius: 4px;
+  width: 250px;
+  border-style: none;
+  height: 20px;
+}
+
+.form {
+  margin-top: 20px;
+  margin-left: 10px;
 }
 
 .fechar {
-    background-color: #2E4756;
-    width: 30px;
-    height: 30px;
-    float: right;
-    cursor: pointer;
-    margin-right: 40px;
-    margin-top: 180px;
-    border: 1px solid #2E4756;
+  background-color: #2E4756;
+  width: 30px;
+  height: 30px;
+  float: right;
+  cursor: pointer;
+  margin-right: 40px;
+  margin-top: 300px;
+  border: 1px solid #2E4756;
 
 }
 
 .modal {
-    margin-top: 50px;
-    width: 500px;
-    height: 500px;
-    position: absolute;
-    display: none;
-    background-color: #2E4756;
-    animation: animate;
-    animation-duration: 800ms;
-    border-radius: 20px;
+  margin-top: 50px;
+  width: 500px;
+  height: 500px;
+  position: absolute;
+  display: none;
+  background-color: #2E4756;
+  animation: animate;
+  animation-duration: 800ms;
+  border-radius: 20px;
 
 }
-@keyframes animate {
-    from {
-        opacity: 1;
-    }
 
-    from {
-        opacity: 0;
-    }
+@keyframes animate {
+  from {
+    opacity: 1;
+  }
+
+  from {
+    opacity: 0;
+  }
 }
 
 #button-add {
@@ -188,14 +204,7 @@ export default {
 
 #button-addA {
   /* right: 50px; */
-  margin-top: 20px;
-  top: 150px;
-  display: flex;
-  position: fixed;
-  text-align: center;
-  align-items: center;
   font-size: 15px;
-  margin-left: 300px;
 
 }
 
